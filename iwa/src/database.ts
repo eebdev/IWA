1cf827d7924518ca7e7a3f0466d492f65b7c3e97
Reinder Noordmans <85369967+ReinderN@users.noreply.github.com>
2023-03-24 09:37:07 +0100

    Update database.ts
    Documentatie is nu toegevoegd en foutafhandeling voor als we daar later nog iets mee moeten doen/

--- iwa/src/database.ts
+++ iwa/src/database.ts
@@ -1,5 +1,10 @@
+// Importeer de benodigde mysql2/promise module
 import mysql from 'mysql2/promise';
 
+/**
+ * Creëer een MySQL-verbinding pool met behulp van de configuratie uit
+ * de omgevingsvariabelen.
+ */
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -7,12 +12,26 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 
+/**
+ * Voert een MySQL-query uit met behulp van de opgegeven SQL-string en waarden, en retourneert de resultaten.
+ * @param {string} sql - De SQL-query string.
+ * @param {any[]} [values=[]] - Een optionele array met waarden die in de query moeten worden ingevoegd.
+ * @returns {Promise<any>} - Een Promise die resulteert in de queryresultaten.
+ */
 export async function query<T = any>(sql: string, values: any[] = []): Promise<any> {
+  // Verkrijg een verbinding uit de pool
   const connection = await pool.getConnection();
   try {
-    const [rows] = await connection.query(sql, values);
+    // Voer de query uit met behulp van de opgegeven SQL-string en waarden
+    const [rows] = await connection.query<T>(sql, values);
+    // Retourneer de queryresultaten
     return rows;
+  } catch (error) {
+    // Log de fout en gooi de fout opnieuw om te worden afgehandeld door de aanroepende functie
+    console.error('Database query error:', error);
+    throw error;
   } finally {
+    // Geef de verbinding terug aan de pool
     connection.release();
   }
 }
