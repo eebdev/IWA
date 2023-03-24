36d2a353c2158b962c2b3bd8d8cff5544974d8d3
Reinder Noordmans <85369967+ReinderN@users.noreply.github.com>
2023-03-24 13:33:59 +0100

    Alles verdeeld in verschillende bestanden zodat het overzichtelijker is

--- iwa/src/pages/api/postWeatherData.tsx
+++ iwa/src/pages/api/postWeatherData.tsx
@@ -1,131 +1,6 @@
 // Importeer de benodigde modules en database query-functie
 import { NextApiRequest, NextApiResponse } from "next";
-import { query } from "../../database";
-
-// Definieer een type voor de weerdata
-interface WeatherData {
-  STN: string;
-  DATE: string;
-  TIME: string;
-  TEMP: number;
-  DEWP: number;
-  STP: number;
-  SLP: number;
-  VISIB: number;
-  WDSP: number;
-  PRCP: number;
-  SNDP: number;
-  FRSHTT: string;
-  CLDC: number;
-  WNDDIR: number;
-}
-
-/**
- * Slaat de weerdata op in de database.
- * @param {WeatherData} data - Het dataobject met weerinformatie.
- */
-async function saveToDatabase(data: WeatherData) {
-  // Destructureer de kolommen uit het dataobject
-  const {
-    STN,
-    DATE,
-    TIME,
-    TEMP,
-    DEWP,
-    STP,
-    SLP,
-    VISIB,
-    WDSP,
-    PRCP,
-    SNDP,
-    FRSHTT,
-    CLDC,
-    WNDDIR,
-  } = data;
-
-  // Combineer de datum- en tijdvelden tot een enkele datetime-waarde
-  const datetime = `${DATE} ${TIME}`;
-
-  // Voer een query uit om de weerdata in de database op te slaan
-  await query(
-    `
-    INSERT INTO station_data (station_name, datetime, temp, dewp, stp, slp, visib, wdsp, prcp, sndp, frshtt, cldc, wnddir)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `,
-    [
-      STN,
-      datetime,
-      TEMP,
-      DEWP,
-      STP,
-      SLP,
-      VISIB,
-      WDSP,
-      PRCP,
-      SNDP,
-      FRSHTT,
-      CLDC,
-      WNDDIR,
-    ]
-  );
-}
-
- */
-function isValidWeatherData(data: WeatherData): boolean {
-  return !Object.values(data).includes('None');
-}
-
-/**
- * Slaat de missende weerdata op in de database om deze later te kunnen gebruiken voor interpolatie.
- * @param {WeatherData} data - Het dataobject met weerinformatie.
- * @param {string} column_name - De naam van de kolom waar de data mist.
- */
-async function storeMissingData(data: WeatherData, column_name: string) {
-  // Destructureer de kolommen uit het dataobject
-  const {
-    STN,
-    DATE,
-    TIME,
-    TEMP,
-    DEWP,
-    STP,
-    SLP,
-    VISIB,
-    WDSP,
-    PRCP,
-    SNDP,
-    FRSHTT,
-    CLDC,
-    WNDDIR,
-  } = data;
-
-  // Combineer de datum- en tijdvelden tot een enkele datetime-waarde
-  const datetime = `${DATE} ${TIME}`;
-
-  // Voer een query uit om de weerdata in de database op te slaan
-  await query(
-    `
-    INSERT INTO missing_data (station_name, datetime, temp, dewp, stp, slp, visib, wdsp, prcp, sndp, frshtt, cldc, wnddir, column_name)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `,
-    [
-      STN,
-      datetime,
-      TEMP,
-      DEWP,
-      STP,
-      SLP,
-      VISIB,
-      WDSP,
-      PRCP,
-      SNDP,
-      FRSHTT,
-      CLDC,
-      WNDDIR,
-      column_name,
-    ]
-  );
-}
+import { saveToDatabase, storeMissingData, updateMissingDataIfNeeded } from "@/weatherData";
 
 /**
  * De dataReceiver functie is een async handler voor NextApiRequest en NextApiResponse.
@@ -169,9 +44,9 @@ export default async function dataReceiver(
         } else {
           // Sla het item op in de database
           await saveToDatabase(item);
-        } else {
-          // Log een foutmelding als het item niet geldig is
-          // console.log('Skipping invalid item:', item);
+
+          // Controleer of er missende data is die kan worden bijgewerkt
+          await updateMissingDataIfNeeded(item.STN);
         }
       }
       // Stuur een succesvolle HTTP-status en bericht terug
