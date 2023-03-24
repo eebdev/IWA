2db52be894277194e174390bfa56bfb0d4ca48cd
Reinder Noordmans <85369967+ReinderN@users.noreply.github.com>
2023-03-24 09:18:21 +0100

    None filter

--- iwa/src/pages/api/postWeatherData.tsx
+++ iwa/src/pages/api/postWeatherData.tsx
@@ -35,6 +35,11 @@ export default async function dataReceiver(req: NextApiRequest, res: NextApiResp
       const data = receivedData.WEATHERDATA; // Access the data array
 
       for (const item of data) {
+        // check if any column in the item is None and skip over the entire item if so
+        if (Object.values(item).includes('None')) {
+          console.log('Skipping item:', item);
+          continue;
+        }
         await saveToDatabase(item);
       }
       res.status(200).json({ message: 'Data received and saved successfully' });
