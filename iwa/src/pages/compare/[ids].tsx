fcc5b15831137ef671c50b5d9808224b50336809
Reinder Noordmans <85369967+ReinderN@users.noreply.github.com>
2023-04-06 13:53:24 +0200

    Mooie tabellen bij ids

--- iwa/src/pages/compare/[ids].tsx
+++ iwa/src/pages/compare/[ids].tsx
@@ -5,42 +5,38 @@ import { useState } from "react";
 
 function renderStationData(data: StationData[]) {
   return (
-    <div className="flex flex-row items-center text-center">
-      {data.map((station, index) => (
-        <div key={index} className="flex flex-row items-center justify-center bg-gray-200 rounded-lg p-2 m-2">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Property
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Value
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {Object.entries(station).map(([key, value]) => (
-                <tr key={key}>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {key}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                    {value}
-                  </td>
+      <div className="flex flex-wrap justify-center">
+        {data.map((station, index) => (
+            <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-4">
+              <table className="min-w-full divide-y divide-gray-200 shadow-lg rounded-md">
+                <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Property
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Value
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ))}
-    </div>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                {Object.entries(station).map(([key, value]) => (
+                    <tr key={key}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {key}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {value}
+                      </td>
+                    </tr>
+                ))}
+                </tbody>
+              </table>
+            </div>
+        ))}
+      </div>
   );
 }
-
-
-
-// station/100200?startDate=2004-11-14&endDate=2005-11-14
 
 export default function StationsComparePage() {
   const router = useRouter();
@@ -65,19 +61,38 @@ export default function StationsComparePage() {
   }
 
   return (
-    <>
-      <div className="stations-container grid grid-cols-3 gap-4">
-        <>{data ? renderStationData(data) : <p>Loading station data...</p>}</>
-      </div>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="startDate">Start date </label>
-          <input type="date" id="startDate" name="startDate" />
-          <br></br>
-          <label htmlFor="endDate">End date </label>
-          <input type="date" id="endDate" name="endDate" />
-          <br></br>
-          <button type="submit">Submit</button>
-        </form>
-    </>
+      <>
+        <div className="bg-white min-h-screen p-6">
+          <div className="stations-container">
+            <>{data ? renderStationData(data) : <p>Loading station data...</p>}</>
+          </div>
+          <form className="flex flex-col items-center space-y-4" onSubmit={handleSubmit}>
+            <div className="flex items-center space-x-2">
+              <label htmlFor="startDate">Start date:</label>
+              <input
+                  className="p-2 text-lg border-2 border-gray-300 rounded-md focus:border-blue-300 focus:outline-none"
+                  type="date"
+                  id="startDate"
+                  name="startDate"
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <label htmlFor="endDate">End date:</label>
+              <input
+                  className="p-2 text-lg border-2 border-gray-300 rounded-md focus:border-blue-300 focus:outline-none"
+                  type="date"
+                  id="endDate"
+                  name="endDate"
+              />
+            </div>
+            <button
+                className="w-32 p-2 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none"
+                type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </>
   );
 }
