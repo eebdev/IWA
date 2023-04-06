cbf819d3b0c7b990f54c9ad6123102bbe9100c43
Francois <francoisdiel@hotmail.com>
2023-04-06 14:35:06 +0200

    Hoort bij de vorige

--- iwa/src/pages/station/[id].tsx
+++ iwa/src/pages/station/[id].tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 function renderStationData(data: StationData) {
     return (
         <table className="w-full divide-y divide-gray-200 shadow-lg rounded-md">
-            <thead className="bg-gray-50">
+            <thead className="head-lightblue">
             <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Property
